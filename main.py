b18163fd2d3dfacae516f706bccc2f1b757cddd5
armstjc <70788681+armstjc@users.noreply.github.com>
2022-02-02 12:58:30 -0500

    multithreading rc 1

---
+++ main.py
@@ -0,0 +1,58 @@
+import time
+import threading
+import schedule
+#import queue
+
+from getInstitutionalHolders import getInstitutionalHolders
+from getMajorHolders import getMajorHolders
+from getStockFinancials import getStockFinancials
+from getStockHistory import getStockHistory
+from getStockOptions import getStockOptions
+from getStockRecommendations import getStockRecommendations
+
+def run_threaded(job_func):
+    job_thread = threading.Thread(target=job_func)
+    job_thread.start()
+
+def job1():
+    print("Starting job 1")
+    getInstitutionalHolders()
+
+
+def job2():
+    print("Starting job 2")
+    getMajorHolders()
+
+def job3():
+    print("Starting job 3")
+    getStockFinancials()
+
+def job4():
+    print("Starting job 4")
+    getStockHistory()
+
+def job5():
+    print("Starting job 5")
+    getStockOptions()
+
+def job6():
+    print("Starting job 6")
+    getStockRecommendations()
+
+def worker_main():
+    while 1:
+        job_func = jobqueue.get()
+        job_func()
+        jobqueue.task_done()
+
+
+schedule.every().day.at("11:58").do(run_threaded, job1)
+schedule.every().day.at("11:59").do(run_threaded, job2)
+schedule.every().day.at("12:00").do(run_threaded, job3)
+schedule.every().day.at("12:01").do(run_threaded, job4)
+schedule.every().day.at("12:02").do(run_threaded, job5)
+schedule.every().day.at("12:03").do(run_threaded, job6)
+
+while 1:
+    schedule.run_pending()
+    time.sleep(1)
